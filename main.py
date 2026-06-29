d70b9f6146b12416cba2ba444a0a95a5f8f9a4d3
Leah Sulkowska <buba@Leahs-MacBook-Pro.local>
2026-06-29 13:25:42 +0100

    format and lint

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-def fib(n):
-    if n == 1:
-        x = 1
-    elif n == 0:
-        x = 1
-    else:
-        x = fib(n - 1) + fib(n - 2)
-    return x
-
-
-# print(fib(int(input("Number in sequence: "))))
+def fib(n: int) -> int:
+    return 1 if n in [1, 0] else fib(n - 1) + fib(n - 2)
--- main_test.py
+++ main_test.py
@@ -1,13 +1,14 @@
-from main import fib
 import pytest
 
+from main import fib
 
-def test_fib_one():
+
+def test_fib_one() -> None:
     assert fib(1) == 1
     assert fib(0) == 1
 
 
-def test_fib_other():
+def test_fib_other() -> None:
     assert fib(4) == 5
     assert fib(5) == 8
     assert fib(6) == 13
