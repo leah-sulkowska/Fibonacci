8fe29cbfb9f40751b51449b3373bb068017e38e5
Leah Sulkowska <buba@Leahs-MacBook-Pro.local>
2026-06-29 09:45:31 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n == 1:
+        x = 1
+    elif n == 0:
+        x = 1
+    else:
+        x = fib(n-1) + fib(n-2)
+    return x
+
+#print(fib(int(input("Number in sequence: "))))
+
+
---
+++ main_test.py
@@ -0,0 +1,15 @@
+from main import fib
+import pytest
+
+def test_fib_one():
+    assert fib(1) == 1
+    assert fib(0) == 1
+
+def test_fib_other():
+    assert fib(4) == 5
+    assert fib(5) == 8
+    assert fib(6) == 13
+
+
+if __name__ == '__main__':
+    pytest.main()
